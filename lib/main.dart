bee655ea53e058a88c7dab1a4faf469d0a96d636
Axel460 <axelrob07@gmail.com>
2025-08-18 21:01:19 -0600

    ARGL_GeneralView, agg event_form_view.dart to Home_View.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'view/event_form_view.dart';
+import 'view/Home_View.dart';
 
 void main() {
   runApp(const MyApp());
@@ -28,7 +28,7 @@ class MyApp extends StatelessWidget {
           ),
         ),
       ),
-      home: const EventFormView(),
+      home: HomePage(),
     );
   }
 }
